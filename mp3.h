2849f363022c6083f037208ddad5ea59bd066d75
pyroesp <nicolaslopezesp@gmail.com>
2024-07-16 16:39:38 +0200

    first commit for MP3 player + USART

---
+++ mp3.h
@@ -0,0 +1,18 @@
+/*
+ *  MP3 lib
+ *  pyroesp
+ */
+
+#ifndef __MP3_H
+#define	__MP3_H
+
+#include <stdint.h>
+#include "usart.h"
+
+// IO struct
+typedef struct{
+    Usart com;
+}Mp3;
+
+#endif
+
---
+++ usart.h
@@ -0,0 +1,21 @@
+/*
+ *  USART lib
+ *  pyroesp
+ */
+
+#ifndef __USART_H
+#define	__USART_H
+
+#include <stdint.h>
+
+// USART struct
+typedef struct{
+    void (*usart_flush_rx)(void);
+    uint8_t (*usart_check_rx_overflow)(void);
+    uint8_t (*usart_check_rx_complete)(void);
+    uint8_t (*usart_check_tx_complete)(void);
+    void (*usart_write)(uint8_t v);
+}Usart;
+
+#endif
+
